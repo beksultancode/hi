3b5835198f52362bd502c3f0ae9d2105984fe25d
BeksultanCoder <beksultancs@gmail.com>
2023-12-02 02:59:08 +0600

    huh

--- src/main/java/io/github/beksultancode/Beksultan.java
+++ src/main/java/io/github/beksultancode/Beksultan.java
@@ -1,7 +1,19 @@
 package io.github.beksultancode;
 
+/**
+ * This is a class that says hi.
+ */
 public class Beksultan {
 
+    /**
+     * This is a constructor.
+     */
+    public Beksultan() {
+    }
+
+    /**
+     * This is a method that says hi.
+     */
     public static void sayHi() {
         System.out.println("Hi, from Beksultan!");
     }
